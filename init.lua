e9b7fb926b53dcf38b2e419925aa583b175ec583
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-04-12 19:29:30 +0100

    cursed commit ngl

--- init.lua
+++ init.lua
@@ -2,17 +2,54 @@ require "core"
 require "plugins"
 require "lsp"
 
---- music maybe
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.fn.jobstart(
-      { "mpv", "--no-video", "--loop", vim.fn.expand "~/.config/nvim/assets/lofi.mp3" },
-      { detach = true }
-    )
-  end,
-})
---- debug keys
-vim.notify("TOKEN = " .. tostring(os.getenv("GITHUB_TOKEN")), vim.log.levels.INFO)
+vim.env.PATH = vim.env.PATH .. ':/opt/homebrew/bin:/usr/local/bin'
+-- Music player using mpv
+-- Music player using mpv (simplified, stable)
+local function play_music(track)
+  -- Kill any existing mpv (ignore errors)
+  vim.fn.system("pkill -f mpv")
+
+  -- Expand path & play track
+  local full_path = vim.fn.expand(track)
+  local result = vim.fn.jobstart({ "mpv", "--no-video", "--loop", full_path }, { detach = true })
+
+  if result <= 0 then
+    vim.notify("💥 Failed to start music!", vim.log.levels.ERROR)
+  else
+    vim.notify("🎶 Now playing: " .. full_path)
+  end
+end
+
+
+-- 🎧 Lofi
+vim.api.nvim_create_user_command("PlayLofi", function()
+  play_music(vim.fn.expand("~/.config/nvim/assets/lofi.mp3"))
+end, {})
+
+-- 🌙 Nightcore
+vim.api.nvim_create_user_command("PlayNightcore", function()
+  play_music(vim.fn.expand("~/.config/nvim/assets/nightcore.mp3"))
+end, {})
+
+-- 🍑 Hentai Sounds (peak creativity?)
+vim.api.nvim_create_user_command("PlayDegenerate", function()
+  play_music(vim.fn.expand("~/.config/nvim/assets/yamete-kudasai.mp3"))
+end, {})
+
+-- 🛑 Pause / Resume
+vim.api.nvim_create_user_command("PauseMusic", function()
+  vim.fn.system("pkill -STOP mpv")
+end, {})
+
+vim.api.nvim_create_user_command("PlayMusic", function()
+  vim.fn.system("pkill -CONT mpv")
+end, {})
+
+-- 💀 Stop everything
+vim.api.nvim_create_user_command("StopMusic", function()
+  vim.fn.system("pkill mpv")
+end, {})
+
 
 --- auto stop the damn music after we quit nvim
 vim.api.nvim_create_autocmd("VimLeavePre", {
@@ -21,13 +58,6 @@ vim.api.nvim_create_autocmd("VimLeavePre", {
   end,
 })
 
-vim.api.nvim_create_user_command("PauseMusic", function()
-  vim.fn.system("pkill -STOP mpv")
-end, {})
-
-vim.api.nvim_create_user_command("PlayMusic", function()
-  vim.fn.system("pkill -CONT mpv")
-end, {})
 --- doggo
 vim.api.nvim_create_user_command("Doggo", function()
   vim.cmd("PetsNew dog")
