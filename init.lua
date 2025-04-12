7373b74f9d85e1cc5a0e7c76e2e1e3fb998d2d45
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-04-12 15:18:40 +0100

    music

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
 require "core"
 require "plugins"
 require "lsp"
+
+--- music maybe
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.fn.jobstart(
+      { "mpv", "--no-video", "--loop", vim.fn.expand "~/.config/nvim/assets/lofi.mp3" },
+      { detach = true }
+    )
+  end,
+})
