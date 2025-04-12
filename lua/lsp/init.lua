fa007ae8bd2c59d10ee819d6cecb9614a3c59fc8
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-04-12 16:34:25 +0100

    lua line & auto format on save

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,3 +1,13 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("lspconfig").lua_ls.setup {}
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,5 @@
 require "plugins.lazy"
 require "plugins.rosepine"
 require "plugins.alpha"
+require("plugins.lualine")
+require("nvim-tree").setup()
