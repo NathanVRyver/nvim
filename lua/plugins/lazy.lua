2abb9f137b109ee5e3383d0af880ff81acb9e6ef
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-04-12 15:34:12 +0100

    utils & plugins

---
+++ lua/plugins/lazy.lua
@@ -0,0 +1,21 @@
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup {
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "nvim-tree/nvim-tree.lua" },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-lualine/lualine.nvim" },
+  { "nvim-tree/nvim-web-devicons" },
+  { "goolord/alpha-nvim" },
+  { "folke/which-key.nvim" },
+}
