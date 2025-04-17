0ee13d26790b95e9333a150d60521ed26a31d5a7
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-04-17 14:35:56 +0100

    config telescope to ignore irrelevent dirs

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -12,10 +12,37 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup {
-  { "rose-pine/neovim",                 name = "rose-pine" },
+  { "rose-pine/neovim",       name = "rose-pine" },
   { "nvim-tree/nvim-tree.lua" },
-  { "nvim-telescope/telescope.nvim",    dependencies = { "nvim-lua/plenary.nvim" } },
-  { "nvim-lualine/lualine.nvim" },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            "venv/", "node_modules/", "build/", "dist/", "%.lock", "%.git/", "__pycache__/",
+            "%.svg", "%.png", "%.jpg", "%.jpeg", "%.ico"
+          },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--glob", "!.git/",
+            "--glob", "!venv/",
+            "--glob", "!node_modules/",
+            "--glob", "!build/"
+          },
+        },
+      })
+    end
+  }
+  , { "nvim-lualine/lualine.nvim" },
   { "nvim-tree/nvim-web-devicons" },
   { "goolord/alpha-nvim" },
   { "folke/which-key.nvim" },
