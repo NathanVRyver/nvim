a1c2b93cb7c5be4d1ad297786748264ac96d6799
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-05-15 21:31:25 +0100

    forgor

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,17 +1,25 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup_handlers({
+  function(server_name)
+    require("lspconfig")[server_name].setup({})
+  end,
+})
+
 require("lspconfig").lua_ls.setup {}
+
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function()
     vim.lsp.buf.format()
   end,
 })
+
 local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
-
 
 vim.lsp.handlers["textDocument/hover"] =
     vim.lsp.with(vim.lsp.handlers.hover, { border = require("utils").border() })
